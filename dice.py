3096ef40654c5951c6ff4da20a2de51376e539d4
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-08-17 00:28:25 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,16 +1,39 @@
 n=int(input("Enter a number"))
 if n==1:
-    print("+-----+")
-    print("|  o  |")
-    print("+-----+")
+    print("+---------+")
+    print("|         |")
+    print("|    o    |")
+    print("|         |")
+    print("+---------+")
 elif n==2:
-    print("+-----+")
-    print("| o   |")
-    print("|  o  |")
-    print("+-----+")
-elif n==2:
-    print("+-----+")
-    print("| o   |")
-    print("|  o  |")
-    print("+-----+")
-       
+    print("+---------+")
+    print("|o        |")
+    print("|         |")
+    print("|        o|")
+    print("+---------+")
+elif n==3:
+    print("+---------+")
+    print("|o        |")
+    print("|    o    |")
+    print("|        o|")
+    print("+---------+")
+elif n==4:
+    print("+---------+")
+    print("|o       o|")
+    print("|         |")
+    print("|o       o|")
+    print("+---------+")
+elif n==5:
+    print("+---------+")
+    print("|o       o|")
+    print("|    o    |")
+    print("|o       o|")
+    print("+---------+")
+elif n==6:
+    print("+---------+")
+    print("|o       o|")
+    print("|o       o|")
+    print("|o       o|")
+    print("+---------+")
+else:
+    print("Error")
