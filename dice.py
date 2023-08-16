004a77235b2f910494abac718a43f6158138d600
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-08-16 16:44:29 +0530

    .........

---
+++ dice.py
@@ -0,0 +1,16 @@
+n=int(input("Enter a number"))
+if n==1:
+    print("+-----+")
+    print("|  o  |")
+    print("+-----+")
+elif n==2:
+    print("+-----+")
+    print("| o   |")
+    print("|  o  |")
+    print("+-----+")
+elif n==2:
+    print("+-----+")
+    print("| o   |")
+    print("|  o  |")
+    print("+-----+")
+       
