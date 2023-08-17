dc2218882c09a4a85d632a1146c54b35287aba85
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-08-17 16:41:19 +0530

    ................

---
+++ mcq.py
@@ -0,0 +1,31 @@
+sc = 0
+print("Q1.which animal lay eggs")
+print("a.dog, b.cat, c.duck , d.sheep")
+ans_1 = input("enter you answer")
+if ans_1=="c":
+    sc = sc + 1
+
+print("Q2.A male cow is called?")
+print("a.ox, b.dog, c.sheep, d.monkey")
+ans_2 = input("enter you answer")
+if ans_2=="a":
+    sc = sc + 1
+    
+print("Q3. what is earth only natural satellite")
+print("a.sun, b.moon, c.venus, d.mercury")
+ans_3 = input("enter you answer")
+if ans_3=="b":
+    sc = sc + 1
+
+print("Q4.which part of the body helps you to move")
+print("a.eyes, b.lungs, c.pancrease, d.muscles")
+ans_4 = input("enter you answer")
+if ans_4=="d":
+    sc = sc + 1
+
+print("Q5.which organs cover the entire body and protects it?")
+print("a.liver, b.heart, c.skin, d.brain")
+ans_5 = input("enter you answer")
+if ans_5=="c":
+    sc = sc + 1
+print("The total score is:",sc)
