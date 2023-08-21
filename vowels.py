d7d6fd8f2b3b9afc9f91e68dc048acb589a1672c
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-08-21 23:03:44 +0530

    ...................

---
+++ vowels.py
@@ -0,0 +1,7 @@
+vowel=0
+a=input("Enter a string")
+for i in range (0,len(a)):
+    if(a[i]=="a" or a[i]=="e" or a[i]=="i" or a[i]=="o" or a[i]=="u"):
+        vowel=vowel+1
+   print("the no of vowels",vowel)
+    
