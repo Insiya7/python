2ac6509d205f4d1f7389a299fb8767ac972b6071
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-08-21 21:45:56 +0530

    ............

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a no"))
+if n%3==0 and n%5==0:
+    print("FizzBuzz")
+elif n%3==0:
+    print("fizz")
+elif n%5==0:
+    print("buzz")
+else:
+    print("no is invalid")
+    
+
+
