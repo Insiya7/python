d8ff7205380af4c8ce51a5ec10e467f447a19090
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-11-08 15:31:57 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+#Creating dictionary for demographic data....
+d=int(input("enter age"))
+d1=input("enter gender")
+d2=input("enter location")
+d3=input("enter martial status")
+print({"age":d,"gender":d1,"location":d2,"martial status":d3})
+#Creating dictionary for course data....
+c=int(input("enter course id"))
+c1=input("enter course name")
+c2=input("enter faculty")
+print({"course id":c,"course name":c1, "faculty":c2})
+
