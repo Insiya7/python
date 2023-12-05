0460ddcb53c4adbe4056e88c075b62c9d756fed7
Insiya7 <125920757+Insiya7@users.noreply.github.com>
2023-12-05 17:02:37 +0530

    .....

---
+++ sum_n.py
@@ -0,0 +1,15 @@
+'''def sum_n(n):    
+    x=0
+    for i in range(0,n+1):
+        x=x+i
+    print(x)
+
+a = 35
+sum_n(9,10)'''
+
+def factorial(n):
+    x=1
+    for i in range(1,n+1):
+        x=x*i
+    print(x)
+sum_n(5)    
